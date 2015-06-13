83d82fd2aa1f277f58a47684b96bff1a02edceaa
James Ottaway <git@james.ottaway.io>
2015-06-14 09:25:13 +1000

    Implement the 'Reset' instruction, but don't expose it via the CLI

---
+++ lib/yubioath/reset.rb
@@ -0,0 +1,17 @@
+require 'yubioath'
+require 'bindata'
+
+module YubiOATH
+  class Reset
+    def self.send(to:)
+      ::YubiOATH::Response.read(to.transmit(Request.new.to_binary_s))
+    end
+
+    class Request < BinData::Record
+      uint8 :cla, value: CLA
+      uint8 :ins, value: 0x04
+      uint8 :p1, value: 0xDE
+      uint8 :p2, value: 0xAD
+    end
+  end
+end
