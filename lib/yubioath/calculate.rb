abef87baf6fd2239b8d4bcc122c72b14177e32f6
James Ottaway <git@james.ottaway.io>
2015-06-13 19:08:49 +1000

    Implement the 'Calculate' instruction

---
+++ lib/yubioath/calculate.rb
@@ -0,0 +1,48 @@
+require 'yubioath'
+require 'bindata'
+
+module YubiOATH
+  class Calculate
+    def self.send(name:, timestamp:, to:)
+      data = Request::Data.new(name: name, timestamp: timestamp.to_i / 30)
+      request = Request.new(data: data.to_binary_s)
+      response = ::YubiOATH::Response.read(to.transmit(request.to_binary_s))
+      throw unless response.success?
+      Response.read(response.data)
+    end
+
+    class Request < BinData::Record
+      uint8 :cla, value: CLA
+      uint8 :ins, value: 0xA2
+      uint8 :p1, value: 0x00
+      uint8 :p2, value: 0x01
+      uint8 :data_length, value: -> { data.length }
+      string :data
+
+      class Data < BinData::Record
+        uint8 :name_tag, value: 0x71
+        uint8 :name_length, value: -> { name.length }
+        string :name
+
+        uint8 :challenge_tag, value: 0x74
+        uint8 :challenge_length, value: 8
+        uint64be :timestamp
+      end
+    end
+
+    class Response < BinData::Record
+      class Code < BinData::Record
+        uint8 :digits
+        uint32be :response
+
+        def to_s
+          (response % 10**digits).to_s.rjust(digits, '0')
+        end
+      end
+
+      uint8 :response_tag
+      uint8 :response_length
+      code :code, read_length: :response_length
+    end
+  end
+end
