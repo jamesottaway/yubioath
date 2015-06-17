8b1706d0f4a80ee8acec34bcd4ae2d5eb227c425
James Ottaway <git@james.ottaway.io>
2015-06-17 21:57:51 +1000

    Move the general Response class into its own file

--- lib/yubioath.rb
+++ lib/yubioath.rb
@@ -1,16 +1,7 @@
 require 'bindata'
+require 'yubioath/response'
 
 module YubiOATH
   CLA = 0x00
   AID = [0xA0, 0x00, 0x00, 0x05, 0x27, 0x21, 0x01, 0x01]
-
-  class Response < BinData::Record
-    count_bytes_remaining :data_length
-    string :data, read_length: -> { data_length - 2 }
-    array :success, type: :uint8, initial_length: 2
-
-    def success?
-      success == [0x90, 0x00]
-    end
-  end
 end
---
+++ lib/yubioath/response.rb
@@ -0,0 +1,13 @@
+require 'bindata'
+
+module YubiOATH
+  class Response < BinData::Record
+    count_bytes_remaining :data_length
+    string :data, read_length: -> { data_length - 2 }
+    array :success, type: :uint8, initial_length: 2
+
+    def success?
+      success == [0x90, 0x00]
+    end
+  end
+end
