72d2a488245cb02ff96d2e13359306aadfdbc978
Tom Haynes <tom.haynes@itv.com>
2018-05-14 17:41:24 +0100

    correct generated challenge, and add mutual verification check

--- lib/yubioath/validate.rb
+++ lib/yubioath/validate.rb
@@ -16,21 +16,24 @@ class YubiOATH
       key = OpenSSL::PKCS5.pbkdf2_hmac_sha1(pin, select_response.name.to_binary_s, 1000, 16)
 
       # compute hash of key & challenge for response
-      r = OpenSSL::HMAC.new(key, OpenSSL::Digest.new('sha1'))
-      r.update(select_response.challenge.to_binary_s)
-      validate_response = r.digest
+      h = OpenSSL::HMAC.new(key, OpenSSL::Digest.new('sha1'))
+      h.update(select_response.challenge.to_binary_s)
+      validate_response = h.digest
+      h.reset
 
-      # compute challenge for mutual validation
-      challenge_seed = Random.new.bytes(8)
-      c = OpenSSL::HMAC.new(key, OpenSSL::Digest.new('sha1'))
-      c.update(challenge_seed)
-      challenge = c.digest
+      # generate challenge for mutual verification
+      challenge = Random.new.bytes(8)
+      h.update(challenge)
+      verification = h.digest
 
       data = Request::Data.new(response: validate_response, challenge: challenge)
       request = Request.new(data: data.to_binary_s)
       bytes = @card.transmit(request.to_binary_s)
-      response = Response.read(bytes)
 
+      response = Response.read(bytes)
+      unless response.response == verification
+        fail 'Response from validation does not match verification!'
+      end
     end
 
     class Request < BinData::Record
@@ -47,16 +50,15 @@ class YubiOATH
         string :response
 
         uint8 :challenge_tag, value: 0x74
-        uint8 :challenge_length, value: -> { verification.length }
-        string :verification
+        uint8 :challenge_length, value: -> { challenge.length }
+        string :challenge
       end
     end
 
     class Response < BinData::Record
-      # we don't seem to be getting the full response expected by https://developers.yubico.com/OATH/YKOATH_Protocol.html
-      uint8 :response_tag
-      # uint8 :response_length
-      # string :response, type: :uint8, read_length: :response_length
+      uint8 :response_tag, assert: 0x75
+      uint8 :response_length
+      string :response, type: :uint8, read_length: :response_length
     end
   end
 end
